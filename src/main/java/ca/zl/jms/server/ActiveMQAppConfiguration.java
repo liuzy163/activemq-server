f73bdf8fec7ed8aa5dad2a1f08f53564f6685f48
liuzy163 <zhiyongliu2013@gmail.com>
2016-09-26 14:32:14 -0400

    Changed to use LeaseDatabaseLocker to improve the recovery from database/network failover

--- src/main/java/ca/zl/jms/server/ActiveMQAppConfiguration.java
+++ src/main/java/ca/zl/jms/server/ActiveMQAppConfiguration.java
@@ -6,7 +6,9 @@ import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.openwire.OpenWireFormat;
 import org.apache.activemq.store.PersistenceAdapter;
 import org.apache.activemq.store.jdbc.JDBCPersistenceAdapter;
+import org.apache.activemq.store.jdbc.LeaseDatabaseLocker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -14,14 +16,25 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource("classpath:my-beans.xml")
 public class ActiveMQAppConfiguration {
-	@Autowired
-	private BrokerService broker;
+  @Autowired
+  private BrokerService broker;
 
-	@Bean
-	public PersistenceAdapter persistenceAdapter(DataSource dataSource) throws Exception {
-		JDBCPersistenceAdapter jdbcPersistenceAdapter = new JDBCPersistenceAdapter(dataSource, new OpenWireFormat());
-		broker.setPersistenceAdapter(jdbcPersistenceAdapter);
-		broker.start();
-		return jdbcPersistenceAdapter;
-	}
+  @Bean
+  public PersistenceAdapter persistenceAdapter(DataSource dataSource,
+      @Value("${activemq.broker.name}") String brokerName) throws Exception {
+    JDBCPersistenceAdapter jdbcPersistenceAdapter =
+        new JDBCPersistenceAdapter(dataSource, new OpenWireFormat());
+    broker.setPersistenceAdapter(jdbcPersistenceAdapter);
+    LeaseDatabaseLocker locker = new LeaseDatabaseLocker();
+    locker.setDataSource(dataSource);
+    locker.setLeaseHolderId(brokerName);
+    // TODO: externalize the value into application.properties
+    //This value must be greater than the LockKeepAlivePeriod
+    locker.setLockAcquireSleepInterval(5000);
+    jdbcPersistenceAdapter.setLocker(locker);
+    // TODO: externalize the value into application.properties
+    jdbcPersistenceAdapter.setLockKeepAlivePeriod(1000);
+    broker.start();
+    return jdbcPersistenceAdapter;
+  }
 }
